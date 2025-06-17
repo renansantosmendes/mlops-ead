616edd9be29d57d47cfc800fae75be2f347513b3
Renan Santos Mendes <renansantosmendes@gmail.com>
2025-06-16 23:33:48 -0300

    update files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 import os
-import json
 import mlflow
-import uvicorn
 import numpy as np
 from pydantic import BaseModel
 from fastapi import FastAPI
@@ -28,8 +26,22 @@ app = FastAPI(title="Fetal Health API",
 
 
 def load_model():
+    """
+    Loads a pre-trained model from an MLflow server.
+
+    This function connects to an MLflow server using the provided tracking URI, username,
+     and password.
+    It retrieves the latest version of the 'fetal_health' model registered on the server.
+    The function then loads the model using the specified run ID and returns the loaded model.
+
+    Returns:
+        loaded_model: The loaded pre-trained model.
+
+    Raises:
+        None
+    """
     print('reading model...')
-    MLFLOW_TRACKING_URI = 'https://dagshub.com/renansantosmendes/mlops-ead.mlflow'
+    MLFLOW_TRACKING_URI = 'https://dagshub.com/renansantosmendes/mlops-ead-2025.mlflow'
     MLFLOW_TRACKING_USERNAME = 'renansantosmendes'
     MLFLOW_TRACKING_PASSWORD = 'b63baf8c662a23fa00deb74ba86600278769e5dd'
     os.environ['MLFLOW_TRACKING_USERNAME'] = MLFLOW_TRACKING_USERNAME
@@ -50,6 +62,16 @@ def load_model():
 
 @app.on_event(event_type='startup')
 def startup_event():
+    """
+    A function that is called when the application starts up. It loads a model into the
+    global variable `loaded_model`.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     global loaded_model
     loaded_model = load_model()
 
@@ -57,22 +79,39 @@ def startup_event():
 @app.get(path='/',
          tags=['Health'])
 def api_health():
+    """
+    A function that represents the health endpoint of the API.
+
+    Returns:
+        dict: A dictionary containing the status of the API, with the key "status" and
+        the value "healthy".
+    """
     return {"status": "healthy"}
 
 
 @app.post(path='/predict',
           tags=['Prediction'])
 def predict(request: FetalHealthData):
+    """
+    Predicts the fetal health based on the given request data.
+
+    Args:
+        request (FetalHealthData): The request data containing the fetal health parameters.
+
+    Returns:
+        dict: A dictionary containing the prediction of the fetal health.
+
+    Raises:
+        None
+    """
     global loaded_model
     received_data = np.array([
         request.accelerations,
         request.fetal_movement,
         request.uterine_contractions,
         request.severe_decelerations,
-    ]).reshape(1, -1)
+    ], dtype=np.float32).reshape(1, -1)
     print(received_data)
     prediction = loaded_model.predict(received_data)
     print(prediction)
     return {"prediction": str(np.argmax(prediction[0]))}
-
-
--- test_train.py
+++ test_train.py
@@ -1,23 +1,21 @@
-import os
-import pytest
-import tensorflow as tf
 import pandas as pd
-import numpy as np
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
+import pytest
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, InputLayer
 
-from train import (reset_seeds,
-                   read_data,
-                   process_data,
+from train import (read_data,
                    create_model,
-                   config_mlflow,
                    train_model)
 
 
 @pytest.fixture
 def sample_data():
+    """
+    A fixture function that returns a sample dataset.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing sample data with three columns: 'feature1',
+         'feature2', and 'fetal_health'.
+    """
     data = pd.DataFrame({
         'feature1': [1, 2, 3, 4, 5],
         'feature2': [6, 7, 8, 9, 10],
@@ -27,6 +25,16 @@ def sample_data():
 
 
 def test_read_data():
+    """
+    This function tests the `read_data` function. It checks whether the returned data is not
+     empty for both features (X) and labels (y).
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     X, y = read_data()
 
     assert not X.empty
@@ -34,6 +42,10 @@ def test_read_data():
 
 
 def test_create_model():
+    """
+    Generate the function comment for the given function body in a markdown code block with
+    the correct language syntax.
+    """
     X, _ = read_data()
     model = create_model(X)
 
@@ -43,6 +55,17 @@ def test_create_model():
 
 
 def test_train_model(sample_data):
+    """
+    Generate a function comment for the given function body in a markdown code block with
+    the correct language syntax.
+
+    Parameters:
+        sample_data (pandas.DataFrame): The input data containing features and target
+        variable.
+
+    Returns:
+        None
+    """
     X = sample_data.drop(['fetal_health'], axis=1)
     y = sample_data['fetal_health'] - 1
     model = create_model(X)
